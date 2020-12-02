c4cf7f4340cef34274bee7cb5141966c8dd48ef4
henrySwin <hanstis@outlook.com>
2020-12-02 15:12:23 +1100

    Add Length functionality in JavaScript
    Checks which RadioButton has been clicked and assigns the result to the 'length' variable. The default length is 16.

--- custom.js
+++ custom.js
@@ -1,9 +1,25 @@
 "use strict";
 
+function getLength() {
+    length = 16;
+    if (document.getElementById("16Length").checked) { length = 16; }
+    else if (document.getElementById("20Length").checked) { length = 20; }
+    else if (document.getElementById("32Length").checked) { length = 32; }
+    else if (document.getElementById("otherLength").checked) {
+        if (!document.getElementById("inputOtherLength").value > 0) { length = 16; }
+        else { length = document.getElementById("inputOtherLength").value; }
+    }
+    else { console.error("getLength() error."); }
+    console.log("Length: " + length);
+    return length;
+}
+
 function formValidate() {
-    document.getElementById("result").value = "Hi.";
+    length = getLength();
+    document.getElementById("result").innerHTML = "Hi.";
+    //document.getElementById("result").style.color = 'red'; // Changes text to red.
 }
 
 window.onload = function() {
-    document.getElementById("form").onclick = formValidate;
+    document.getElementById("generate").onclick = formValidate;
 }

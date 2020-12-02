9085df0d3a1c1ce93d35d7ee44dff6e75c17862a
henrySwin <102578431@student.swin.edu.au>
2020-12-02 23:01:53 +1100

    (JavaScript) Calculate variety of characters with error-checking

--- custom.js
+++ custom.js
@@ -38,38 +38,47 @@ function getLength() {
 }
 
 
-// Ensure that some characters have been selected for generation.
-function checkTypes(aTypes, aCheckboxes) {
-    // Check if any checkboxes have been selected. If not, below expression equals 0.
-    if (aTypes[0] + aTypes[1] + aTypes[2] + aTypes[3] == 0) {
-        for (var i = 0; i < 4; i++) {
-            aTypes[i] = true;
-            document.getElementById(aCheckboxes[i]).checked = true;
-        } 
-    }
-    return aTypes;
-}
-
-
-function getTypes() {
+function getTypes(aCheckboxes) {
     var types = new Array(6);
-    const checkboxes = ["lowercase","uppercase","numbers","symbols","symbol_at","symbol_dot"];
 
     for (var i = 0; i < types.length; i++)
-        types[i] = document.getElementById(checkboxes[i]).checked;
-
-    types = checkTypes(types, checkboxes);
-
-    for (var i = 0; i < types.length; i++)
-        console.info(checkboxes[i] + ": " + types[i]); // e.g. "lowercase: true".
+        types[i] = document.getElementById(aCheckboxes[i]).checked;
 
     return types;
 }
 
 
+function fixTypes(aTypes, aCheckboxes) {
+    // Re-enable the @ and . checkboxes.
+    document.getElementById("symbol_at").disabled = false;
+    document.getElementById("symbol_dot").disabled = false;
+
+    for (var i = 0; i < 4; i++) {
+        aTypes[i] = true;
+        document.getElementById(aCheckboxes[i]).checked = true;
+    }
+    return aTypes;
+}
+
+
 function validate() {
+    const checkboxes = ["lowercase","uppercase","numbers","symbols","symbol_at","symbol_dot"];
     const length = getLength();
-    const types = getTypes();
+    var types = getTypes(checkboxes);
+    var variety = types[0] + types[1] + types[2] + types[3];
+
+    // Ensure that some characters have been selected for generation.
+    // If no checkboxes have been selected, variety == 0.
+    if (variety == 0) {
+        variety = 4;
+        types = fixTypes(types, checkboxes);
+    }
+    
+
+    for (var i = 0; i < types.length; i++)
+        console.info(checkboxes[i] + ": " + types[i]); // e.g. "lowercase: true".
+    
+    console.log("Variety: " + variety);
 
     document.getElementById("result").innerHTML = "Hi.";
     //document.getElementById("result").style.color = 'red'; // Changes text to red.
